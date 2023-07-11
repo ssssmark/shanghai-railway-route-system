3133285fec65daad747621ccd9e79ff1d8b64bfc
ssssmark <111336914+ssssmark@users.noreply.github.com>
2023-07-11 23:56:59 +0800

    Navbar导航栏组件

---
+++ src/navbar.js
@@ -0,0 +1,24 @@
+import "./navbar.css"
+import React from 'react'
+export default class Navbar extends React.Component{
+    state={
+        tabIndex:1
+    }
+    changestate=(activeindex)=> {
+        this.setState({
+            tabIndex:activeindex
+        })
+    }
+    render()
+    {
+        return(
+            <div>
+                <div className="Tab-bar">
+                    <div className="title">MetroPlan</div>
+                    <div className={this.state.tabIndex==1?"selected-tab":"unselected-tab"} id="home" onClick={()=>this.changestate(1)}>Home</div>
+                    <div className={this.state.tabIndex==2?"selected-tab":"unselected-tab"} id="help"onClick={()=>this.changestate(2)}>Help</div>
+                </div>
+            </div>
+        )
+    }
+}
