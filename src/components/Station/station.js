f9324088e9dd39f412e837a2ab211b1a88c1a2c8
ssssmark <111336914+ssssmark@users.noreply.github.com>
2023-07-16 20:28:35 +0800

    地铁线路和站点可视化
    添加了两个组件Line、Station

---
+++ src/components/Station/station.js
@@ -0,0 +1,66 @@
+import React from "react";
+import stations from '../../data/stations.json'
+import transfers from '../../data/transfers.json'
+import transferPath from '../../res/img/transfer.png'
+export  default  class Station extends React.Component{
+    convertShow(e) {
+        const stationInfos = require('../../data/stationInfo.json');
+        const attributes = e.target.attributes;
+        const stationName = this.getAttVal(attributes, 'id') || this.getAttVal(attributes, 'dataid');
+        const x = e.target.x ? e.target.x.baseVal.valueAsString : e.target.cx.baseVal.valueAsString;
+        const y = e.target.y ? e.target.y.baseVal.valueAsString : e.target.cy.baseVal.valueAsString;
+        const position = {
+            x: +x + 100,
+            y: +y + 70
+        };
+        const statId = this.getAttVal(attributes, 'statid');
+        let timesheet = null;
+        timesheet = stationInfos[statId].timesheet;
+        timesheet = this.formatTimesheet(timesheet);
+        const infoCard = {
+            show: !this.state.show,
+            stationName: stationName,
+            stationPosition: position,
+            statId: statId,
+            timesheet: timesheet
+        }
+        if (this.state.lastStation === null) {
+            this.setState({ lastStation: statId });
+        } else if (statId !== this.state.lastStation) {
+            const keys = Object.keys(timesheet);
+            if (timesheet !== null && timesheet.length !== 0) {
+                const currentLine = keys[0];
+                infoCard.currentLine = currentLine;
+            }
+        }
+        const stationInfo = stationInfos[statId];
+        this.props.convertShowInfoCard(infoCard, stationInfo);
+    }
+
+    render() {
+        const stationEles = [];
+        const transferEles = [];
+
+        for (let i = 0; i < stations.length; i++) {
+            const station = stations[i];
+            stationEles.push(
+                <circle cx={station.cx} cy={station.cy} r="5" fill="white" stroke={station.stroke} id={station.id} statid={station.statid} key={station.id + i}></circle>
+            )
+        }
+
+        for (let i = 0; i < transfers.length; i++) {
+            const transfer = transfers[i];
+            transferEles.push(
+                <image x={transfer.x} y={transfer.y} dataid={transfer['data-id']} xlinkHref={transferPath} statid={transfer.statid} key={transfer['data-id']+i}
+                       height="16" width="16"></image>
+            )
+        }
+
+        return (
+            <g onClick={e => this.convertShow(e)}>
+                {stationEles}
+                {transferEles}
+            </g>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import Navbar from './components/Navbar/navbar'
 import Searchbox from "./components/SearchBox/searchbox";
 import SubwayMap from "./components/Map/SubwayMap.js";
+import Line from "./components/Line/Line";
+import Station from "./components/Station/station";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +13,11 @@ root.render(
   <React.StrictMode>
     <Navbar />
       <div className="Home"><Searchbox/>
-          <SubwayMap/></div>
+          <svg className="svg" viewBox="200 200 3000 3000" >
+              <Line/>
+              <Station/>
+          </svg>
+          </div>
 
   </React.StrictMode>
 );
