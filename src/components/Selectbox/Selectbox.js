90ab88e7f6199f161c7e9b264ac49c4ab348aaf7
罗理恒 <1651503727@qq.com>
2023-08-29 18:06:49 +0800

    更换成函数式组件

--- src/components/Selectbox/Selectbox.js
+++ src/components/Selectbox/Selectbox.js
@@ -1,76 +1,61 @@
-import React  from "react";
+import React, {useState}from "react";
 import { allStation} from "../../algorithm";
 import {AutoComplete} from "antd";
 import './Selectbox.css'
-export default class Selectbox extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state={
-            input_value:'',
-            options:allStation.map((station)=>({
-                label: station.id,
-                value: station.id
-            }))
-        }
-    }
-    onSelect = (data) => {
-        this.setState({
-            options:allStation.map((station)=>({
-                label: station.id,
-                value: station.id
-            }))
-        })
+function Selectbox(props){
+
+    const [input_value, setInput] = useState('');
+    const [options, setOptions] = useState(allStation.map((station)=>({
+        label: station.id,
+        value: station.id
+    })))
+
+
+    const onSelect = (data) => {
+        setOptions(allStation.map((station)=>({
+            label: station.id,
+            value: station.id
+        })))
+        console.log('onSelect', data);
     };
-    onChange=(data)=>{
+    const onChange=(data:string)=>{
         console.log('onchange', data);
-        this.setState({input_value:data})
-        this.props.get_station(data)
+        setInput(data);
+        props.get_station(data)
     }
-    resetstate=()=>{
-        this.state={
-           options:allStation.map((station)=>({
-               label: station.id,
-               value: station.id
-           }))
-       }
-    }
-   onSearch = (value: string) => {
-        //从options中间找有没有包含value的,有就返回搜索结果,没有不返回
-        this.resetstate()
-        console.log('onSearch:', value);
-        //遍历options,找到包含value的
-        let result = [];
-        for (let i = 0; i < this.state.options.length; i++) {
-            if (this.state.options[i].value.includes(value)) {
-                result.push(this.state.options[i]);
-            }
+   const onSearch = (data: string) => {
+        //恢复到初始化的options
+        if (data === '') {
+            setOptions(allStation.map((station)=>({
+                label: station.id,
+                value: station.id
+            })))
         }
-        //显示options中result的结果
-        this.setState({
-            options: result
-        })
-
-
+        console.log('onSearch:', data);
     };
-    render()
-    {
-        return(
-            <div>
-                <AutoComplete
-                    className="Selectbox"
-                    value={this.state.input_value}
-                    placeholder="Input station"
-                    allowClear={true}
-                    style={{ width: 300 }}
-                    backfill={true}
-                    onSelect={this.onSelect}
-                    onChange={this.onChange}
-                    onSearch={this.onSearch}
-                    options={this.state.options}
-                />
-            </div>
+    return(
+        <div>
+            <AutoComplete
+                className="Selectbox"
+                value={input_value}
+                placeholder="Input station"
+                allowClear={true}
+                style={{ width: 300 }}
+                backfill={true}
+                onSelect={onSelect}
+                onChange={onChange}
+                onSearch={onSearch}
+                options={options}
+                //自动补全
+                filterOption={(inputValue, option) =>
+                    option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1}
+
+            />
+        </div>
 
 
-        )
-    }
+    )
 }
+
+
+export default Selectbox;
